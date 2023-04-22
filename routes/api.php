303faed93d098378c443a50a5e0a933c30a962a3
Weydans Barros <weydans@hotmail.com>
2023-04-22 18:08:03 -0300

    feat: create login and expense resurce routes

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::middleware('auth:sanctum')->group(function() {
+    Route::apiResource('/v0/expenses', App\Http\Controllers\ExpenseController::class);
+});
+
+Route::post( '/v0/login', [ App\Http\Controllers\LoginController::class, "login" ] );
