74dd92ab53702c7e8e20a13875972850b983e417
Weydans Barros <weydans@hotmail.com>
2023-04-23 10:44:12 -0300

    feat: alter ExpenseController to notify user when an expense is created and implement database transaction

--- app/Http/Controllers/ExpenseController.php
+++ app/Http/Controllers/ExpenseController.php
@@ -7,55 +7,124 @@ use App\Http\Requests\UpdateExpenseRequest;
 use App\Models\Expense;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Notifications\ExpenseCreatedNotification;
+use Illuminate\Support\Facades\Notification;
+use Illuminate\Support\Facades\DB;
 
 class ExpenseController extends Controller
 {
     public function index()
     {
-        return response()->json(Expense::all());
+        try {
+            $user = Auth::user();
+
+            return response()->json($user->expenses, 200);
+
+        } catch (Exception $e) {
+            return response()->json(['error' => 'Something went wrong'], 500);
+        }
     }
 
     public function store(StoreExpenseRequest $request, Expense $expense)
     {
-        $expense->fill($request->all());
+        try {
+            DB::beginTransaction();
+
+            $expense->fill($request->all());
         
-        if ( Auth::user()->cannot('create', $expense)) {
-            abort(403);
-        }
+            if ( Auth::user()->cannot('create', $expense)) {
+                return response()->json(['error' => 'Forbiden'], 403);
+            }
 
-        $expense->save();
-        return response()->json($expense, 201);
+            $expense->save();
+            
+            Notification::send(Auth::user(), new ExpenseCreatedNotification($expense));
+            
+            DB::commit();
+            
+            return response()->json($expense, 201);
+
+        } catch (Exception $e) {
+            DB::rollBack();
+
+            return response()->json(['error' => 'Something went wrong'], 500);
+        }
+    }    
+
+    public function show($id)
+    {
+        try {
+            $expense = Expense::find($id);
+
+            if ( empty( $expense ) ) {
+                return response()->json(['error' => 'Not found'], 404);
+            }
+
+            if ( Auth::user()->cannot('view', $expense) ) {
+                return response()->json(['error' => 'Forbiden'], 403);
+            }
+
+            return response()->json($expense, 200);
+
+        } catch (Exception $e) {
+            return response()->json(['error' => 'Something went wrong'], 500);
+        }
     }
 
-    public function show(Expense $expense)
+    public function update($id, UpdateExpenseRequest $request)
     {
-        if ( Auth::user()->cannot('view', $expense) ) {
-            abort(403);
-        }
+        try {
+            DB::beginTransaction();
 
-        return response()->json($expense);
+            $expense = Expense::find($id);
+
+            if ( empty( $expense ) ) {
+                return response()->json(['error' => 'Not found'], 404);
+            }
+            
+            if ( Auth::user()->cannot('update', $expense) ) {
+                return response()->json(['error' => 'Forbiden'], 403);
+            }
+
+            $expense->fill($request->all());
+            $expense->save();
+
+            DB::commit();
+
+            return response()->json($expense, 200);
+
+        } catch (Exception $e) {
+            DB::rollBack();
+
+            return response()->json(['error' => 'Something went wrong'], 500);
+        }
     }
 
-    public function update(UpdateExpenseRequest $request, Expense $expense)
+    public function destroy($id)
     {
-        $expense->fill($request->all());
+        try {
+            DB::beginTransaction();
 
-        if ( Auth::user()->cannot('update', $expense) ) {
-            return response()->json('Forbiden', 403);
+            $expense = Expense::find($id);
+
+            if ( empty( $expense ) ) {
+                return response()->json(['error' => 'Not found'], 404);
+            }
+
+            if ( Auth::user()->cannot('view', $expense) ) {
+                return response()->json(['error' => 'Forbiden'], 403);
+            }
+
+            $expense->delete();
+
+            DB::commit();
+
+            return response('', 204);
+
+        } catch (Exception $e) {
+            DB::rollBack();
+
+            return response()->json(['error' => 'Something went wrong'], 500);
         }
-
-        $expense->save();
-
-        return response()->json($expense);
-    }
-
-    public function destroy(Expense $expense)
-    {
-        if ( Auth::user()->cannot('view', $expense) ) {
-            abort(403);
-        }
-
-        $expense->delete();
-        return response('', 204);
     }
 }
