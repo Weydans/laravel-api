88e9fabdf708256dcc894bf8fc4acaf2f3b11c60
Weydans Barros <weydans@hotmail.com>
2023-04-23 10:45:42 -0300

    feat: implement relationship in models User and Expense

--- app/Models/Expense.php
+++ app/Models/Expense.php
@@ -4,10 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use App\Models\User;
 
 class Expense extends Model
 {
-    use HasFactory;
+    use HasFactory, Notifiable;
 
     protected $fillable = ['user_id', 'description', 'date', 'value'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
 }
