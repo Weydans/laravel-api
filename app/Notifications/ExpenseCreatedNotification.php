044958e390930e1314c404d697f010dd51566e5f
Weydans Barros <weydans@hotmail.com>
2023-04-23 10:46:58 -0300

    feat: disable default api route from Laravel

---
+++ app/Notifications/ExpenseCreatedNotification.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+use App\Models\Expense;
+
+class ExpenseCreatedNotification extends Notification implements ShouldQueue
+{
+    use Queueable;
+    private $expense;
+
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct(Expense $expense)
+    {
+        $this->expense = $expense;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail(object $notifiable): MailMessage
+    {
+        return (new MailMessage)
+                    ->subject('New expense created')
+                    ->greeting("Hello, {$this->expense->user->name}")
+                    ->line('')
+                    ->line("Description: {$this->expense->description}")
+                    ->line("Date: {$this->expense->date}")
+                    ->line("Value: {$this->expense->value}")
+                    ->line('')
+                    ->line('Thank you for using our application!');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            //
+        ];
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -13,13 +13,15 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
-
+/*
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+*/
+
+
+Route::post( '/v0/login', [ App\Http\Controllers\LoginController::class, "login" ] );
 
 Route::middleware('auth:sanctum')->group(function() {
     Route::apiResource('/v0/expenses', App\Http\Controllers\ExpenseController::class);
 });
-
-Route::post( '/v0/login', [ App\Http\Controllers\LoginController::class, "login" ] );
